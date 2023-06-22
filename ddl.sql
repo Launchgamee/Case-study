6f34ed66f2dd449b61c2d1f489deb0b4e698bf1b
Launchgamee <samm04826@gmail.com>
2023-06-22 16:57:42 -0400

    tbl loc and dep

---
+++ ddl.sql
@@ -0,0 +1,13 @@
+create database case_study
+use case_study
+
+create table [Location] 
+(LocationID int primary key,
+City varchar(50)
+)
+
+create table Department 
+(DepartmentId int primary key,
+name varchar (50),
+locationid int foreign key references [location](locationid)
+)
