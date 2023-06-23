119ada0817218a438a27d2b61cc6a56e0664c7a4
Launchgamee <samm04826@gmail.com>
2023-06-23 11:08:08 -0400

    insert values for department on new branch

--- dml.sql
+++ dml.sql
@@ -4,5 +4,9 @@ insert into [location] values
 (124, 'chicago'),
 (167,'boston')
 
-
+insert into [Department] values 
+(10,'accounting',122),
+(20,'sales',124),
+(30,'resarch',123),
+(40,'operations',167)
 
